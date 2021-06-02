5e9751ef5fb38d89f6f618b28f0dd02e55254d34
Joe McCormick <jmccormi@netapp.com>
2021-06-02 17:37:02 -0500

    Adding a long description and project URLs for PyPI.

--- netapp_dstk_k8s/setup.py
+++ netapp_dstk_k8s/setup.py
@@ -1,3 +1,22 @@
+"""
+The NetApp Data Science Toolkit for Kubernetes is a Python library that makes it
+simple for data scientists and data engineers to perform various data management
+tasks within a Kubernetes cluster, such as provisioning a new Kubernetes
+persistent volume or JupyterLab workspace, near-instantaneously cloning a
+Kubernetes persistent volume or JupterLab workspace, and near-instantaneously
+snapshotting a Kubernetes persistent volume or JupyterLab workspace for
+traceability/baselining. This Python library can function as either a command
+line utility or a library of functions that can be imported into any Python 
+program or Jupyter Notebook.
+
+Refer to the documentation for a full list of available functionality.
+
+"""
+DOCLINES = (__doc__ or '').split("\n")
+
 from setuptools import setup
 
-setup()
+setup(
+    long_description="\n".join(DOCLINES[0:]),
+    long_description_content_type='text/markdown',
+)
--- netapp_dstk_traditional/setup.py
+++ netapp_dstk_traditional/setup.py
@@ -1,3 +1,20 @@
+"""
+The NetApp Data Science Toolkit for Traditional Environments is a Python library
+that makes it simple for data scientists and data engineers to perform various
+data management tasks, such as provisioning a new data volume,
+near-instantaneously cloning a data volume, and near-instantaneously
+snapshotting a data volume for traceability/baselining. This Python library can
+function as either a command line utility or a library of functions that can be 
+imported into any Python program or Jupyter Notebook.
+
+Refer to the documentation for a full list of available functionality.
+
+"""
+DOCLINES = (__doc__ or '').split("\n")
+
 from setuptools import setup
 
-setup()
+setup(
+    long_description="\n".join(DOCLINES[0:]),
+    long_description_content_type='text/markdown',    
+)
