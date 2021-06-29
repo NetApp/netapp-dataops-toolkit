d40d129ac46a2186f16b35157d51dcb3d43fb59c
Oglesby, Michael <Mike.Oglesby@netapp.com>
2021-06-29 19:20:32 -0400

    Update version num

--- netapp_dataops_traditional/netapp_dataops/traditional.py
+++ netapp_dataops_traditional/netapp_dataops/traditional.py
@@ -31,7 +31,7 @@ from tabulate import tabulate
 import yaml
 
 
-__version__ = "2.0.0alpha21"
+__version__ = "2.0.0alpha22"
 
 
 # Using this decorator in lieu of using a dependency to manage deprecation
