3dd52935fcafe8f0f29607df9bdadeafd80772f0
Ahmad, Sufian <Sufian.Ahmad@netapp.com>
2022-04-13 11:56:13 -0500

    Added recommended changes.

--- netapp_dataops_k8s/test_scripts/test_triton.sh
+++ netapp_dataops_k8s/test_scripts/test_triton.sh
@@ -32,7 +32,7 @@ printf "** Starting: Test set B.1. - default namespace, long opts\n\n"
 ### Create workspace 1
 server1_name="b-1-w1"
 model_pvc="model-repo-test"
-command="sudo -E ./netapp_dataops_k8s_cli.py create triton-server --server-name=$server1_name --model-repo-pvc-name=$model_pvc"
+command="./netapp_dataops_k8s_cli.py create triton-server --server-name=$server1_name --model-repo-pvc-name=$model_pvc"
 echo "Running: $command"
 eval $command
 printf "\n"
@@ -44,7 +44,7 @@ printf "\n"
 ### Create workspace 2
 server2_name="b-1-w2"
 model_pvc="model-repo-test"
-command="sudo -E ./netapp_dataops_k8s_cli.py create triton-server --server-name=$server2_name --model-repo-pvc-name=$model_pvc --image=nvcr.io/nvidia/tritonserver:21.11-py3 --memory=512Mi --cpu=0.25"
+command="./netapp_dataops_k8s_cli.py create triton-server --server-name=$server2_name --model-repo-pvc-name=$model_pvc --image=nvcr.io/nvidia/tritonserver:21.11-py3 --memory=512Mi --cpu=0.25"
 echo "Running: $command"
 eval $command
 printf "\n"
@@ -53,7 +53,34 @@ printf "\n"
 proceed_prompt
 printf "\n"
 
+### Use List to check if the servers still exist in default namespace
+command="./netapp_dataops_k8s_cli.py list triton-servers"
+echo "Running: $command"
+eval $command
+printf "\nRetrieving server:\n"
+printf "\n"
+proceed_prompt
+printf "\n"
 
+### Delete Triton Inference instance 1
+command="./netapp_dataops_k8s_cli.py delete triton-server --server-name=$server1_name --force"
+echo "Running: $command"
+eval $command
+printf "\nRetrieving server:\n"
+kubectl get deployment ntap-dsutil-triton-$server1_name -o yaml
+printf "\n"
+proceed_prompt
+printf "\n"
+
+### Delete Triton Inference instance 2
+command="./netapp_dataops_k8s_cli.py delete triton-server --server-name=$server2_name --force"
+echo "Running: $command"
+eval $command
+printf "\nRetrieving server:\n"
+kubectl get deployment ntap-dsutil-triton-$server2_name -o yaml
+printf "\n"
+proceed_prompt
+printf "\n"
 
 ####################################################
 ## Test set B.2. - dsk-test namespace, long opts ##
@@ -65,7 +92,7 @@ namespace="dsk-test"
 server3_name="b-2-w1"
 model_pvc="model-repo-test1"
 namespace="dsk-test"
-command="sudo -E ./netapp_dataops_k8s_cli.py create triton-server --namespace=$namespace --server-name=$server3_name --model-repo-pvc-name=$model_pvc"
+command="./netapp_dataops_k8s_cli.py create triton-server --namespace=$namespace --server-name=$server3_name --model-repo-pvc-name=$model_pvc"
 echo "Running: $command"
 eval $command
 printf "\n"
@@ -78,7 +105,7 @@ printf "\n"
 server4_name="b-2-w2"
 model_pvc="model-repo-test1"
 namespace="dsk-test"
-command="sudo -E ./netapp_dataops_k8s_cli.py create triton-server --namespace=$namespace --server-name=$server4_name --model-repo-pvc-name=$model_pvc --image=nvcr.io/nvidia/tritonserver:21.11-py3 --memory=512Mi --cpu=0.25 --load-balancer"
+command="./netapp_dataops_k8s_cli.py create triton-server --namespace=$namespace --server-name=$server4_name --model-repo-pvc-name=$model_pvc --image=nvcr.io/nvidia/tritonserver:21.11-py3 --memory=512Mi --cpu=0.25 --load-balancer"
 echo "Running: $command"
 eval $command
 printf "\n"
@@ -87,10 +114,22 @@ printf "\n"
 proceed_prompt
 printf "\n"
 
-### List services
-command="kubectl -n $namespace get svc"
+### Delete Triton Inference instance 3
+command="./netapp_dataops_k8s_cli.py delete triton-server --server-name=$server3_name --namespace=$namespace --force"
 echo "Running: $command"
 eval $command
+printf "\nRetrieving server:\n"
+kubectl -n $namespace get deployment ntap-dsutil-triton-$server3_name -o yaml
+printf "\n"
+proceed_prompt
+printf "\n"
+
+### Delete Triton Inference instance 4
+command="./netapp_dataops_k8s_cli.py delete triton-server --server-name=$server4_name --namespace=$namespace --force"
+echo "Running: $command"
+eval $command
+printf "\nRetrieving server:\n"
+kubectl -n $namespace get deployment ntap-dsutil-triton-$server4_name -o yaml
 printf "\n"
 proceed_prompt
 printf "\n"
@@ -105,7 +144,7 @@ namespace="dsk-test"
 server5_name="b-3-w1"
 model_pvc="model-repo-test1"
 namespace="dsk-test"
-command="sudo -E ./netapp_dataops_k8s_cli.py create triton-server -n $namespace -s $server5_name -v $model_pvc"
+command="./netapp_dataops_k8s_cli.py create triton-server -n $namespace -s $server5_name -v $model_pvc"
 echo "Running: $command"
 eval $command
 printf "\n"
@@ -118,7 +157,7 @@ printf "\n"
 server6_name="b-3-w2"
 model_pvc="model-repo-test1"
 namespace="dsk-test"
-command="sudo -E ./netapp_dataops_k8s_cli.py create triton-server -n $namespace -s $server6_name -v $model_pvc -i nvcr.io/nvidia/tritonserver:21.11-py3 -m 512Mi -p 0.25 -b"
+command="./netapp_dataops_k8s_cli.py create triton-server -n $namespace -s $server6_name -v $model_pvc -i nvcr.io/nvidia/tritonserver:21.11-py3 -m 512Mi -p 0.25 -b"
 echo "Running: $command"
 eval $command
 printf "\n"
@@ -127,57 +166,8 @@ printf "\n"
 proceed_prompt
 printf "\n"
 
-
-### List services
-command="kubectl -n $namespace get svc"
-echo "Running: $command"
-eval $command
-printf "\n"
-proceed_prompt
-printf "\n"
-
-### Delete Triton Inference instance 1
-command="sudo -E ./netapp_dataops_k8s_cli.py delete triton-server --server-name=$server1_name --force"
-echo "Running: $command"
-eval $command
-printf "\nRetrieving server:\n"
-kubectl get deployment ntap-dsutil-triton-$server1_name -o yaml
-printf "\n"
-proceed_prompt
-printf "\n"
-
-### Delete Triton Inference instance 2
-command="sudo -E ./netapp_dataops_k8s_cli.py delete triton-server --server-name=$server2_name --force"
-echo "Running: $command"
-eval $command
-printf "\nRetrieving server:\n"
-kubectl get deployment ntap-dsutil-triton-$server2_name -o yaml
-printf "\n"
-proceed_prompt
-printf "\n"
-
-### Delete Triton Inference instance 3
-command="sudo -E ./netapp_dataops_k8s_cli.py delete triton-server --server-name=$server3_name --namespace=$namespace --force"
-echo "Running: $command"
-eval $command
-printf "\nRetrieving server:\n"
-kubectl -n $namespace get deployment ntap-dsutil-triton-$server3_name -o yaml
-printf "\n"
-proceed_prompt
-printf "\n"
-
-### Delete Triton Inference instance 4
-command="sudo -E ./netapp_dataops_k8s_cli.py delete triton-server --server-name=$server4_name --namespace=$namespace --force"
-echo "Running: $command"
-eval $command
-printf "\nRetrieving server:\n"
-kubectl -n $namespace get deployment ntap-dsutil-triton-$server4_name -o yaml
-printf "\n"
-proceed_prompt
-printf "\n"
-
 ### Delete Triton Inference instance 5
-command="sudo -E ./netapp_dataops_k8s_cli.py delete triton-server --server-name=$server5_name --namespace=$namespace --force"
+command="./netapp_dataops_k8s_cli.py delete triton-server --server-name=$server5_name --namespace=$namespace --force"
 echo "Running: $command"
 eval $command
 printf "\nRetrieving server:\n"
@@ -187,7 +177,7 @@ proceed_prompt
 printf "\n"
 
 ### Delete Triton Inference instance 6
-command="sudo -E ./netapp_dataops_k8s_cli.py delete triton-server --server-name=$server6_name --namespace=$namespace --force"
+command="./netapp_dataops_k8s_cli.py delete triton-server --server-name=$server6_name --namespace=$namespace --force"
 echo "Running: $command"
 eval $command
 printf "\nRetrieving server:\n"
@@ -197,7 +187,7 @@ proceed_prompt
 printf "\n"
 
 ### Use List to check if the servers still exist in namespace: "dsk-test"
-command="sudo -E ./netapp_dataops_k8s_cli.py list triton --namespace=$namespace"
+command="./netapp_dataops_k8s_cli.py list triton-servers --namespace=$namespace"
 echo "Running: $command"
 eval $command
 printf "\nRetrieving server:\n"
@@ -205,14 +195,14 @@ printf "\n"
 proceed_prompt
 printf "\n"
 
-### Use List to check if the servers still exist in default namespace
-command="sudo -E ./netapp_dataops_k8s_cli.py list triton"
-echo "Running: $command"
-eval $command
-printf "\nRetrieving server:\n"
-printf "\n"
-proceed_prompt
-printf "\n"
+
+
+
+
+
+
+
+
 
 ###################################
 printf "* Test class B complete!\n"
