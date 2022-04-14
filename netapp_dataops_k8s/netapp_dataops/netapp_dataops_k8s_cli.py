6e9863da310bda03997450218053112fac946110
Oglesby, Michael <Mike.Oglesby@netapp.com>
2022-04-14 18:35:42 -0400

    bugfix- cli now uses correct default jlab image

--- netapp_dataops_k8s/netapp_dataops/netapp_dataops_k8s_cli.py
+++ netapp_dataops_k8s/netapp_dataops/netapp_dataops_k8s_cli.py
@@ -734,7 +734,7 @@ if __name__ == '__main__':
             workspaceSize = None
             namespace = "default"
             storageClass = None
-            workspaceImage = "jupyter/scipy-notebook:latest"
+            workspaceImage = "jupyter/tensorflow-notebook"
             requestNvidiaGpu = None
             requestMemory = None
             requestCpu = None
