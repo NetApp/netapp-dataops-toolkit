51f96dc388e525ef3f02c090da76d550eaa022ae
sm74655 <Sushma.Madam@netapp.com>
2025-12-24 18:32:23 +0530

    setting default cooling threshold value

--- netapp_dataops_traditional/netapp_dataops/traditional/gcnv/replication_management.py
+++ netapp_dataops_traditional/netapp_dataops/traditional/gcnv/replication_management.py
@@ -150,7 +150,7 @@ def create_replication(
         # Build TieringPolicy if provided
         if tiering_enabled:
             destination_params.tiering_policy = netapp_v1.TieringPolicy(
-                cooling_threshold_days=cooling_threshold_days or 0
+                cooling_threshold_days=cooling_threshold_days if cooling_threshold_days is not None else 31
             )
     
         # Construct the replication object
