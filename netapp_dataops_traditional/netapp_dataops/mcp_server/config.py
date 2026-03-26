43cf9445f0b8e109dd9cdd622cff1c09a88ffc63
Karnik, Aarkin <Aarkin.Karnik@netapp.com>
2026-03-26 15:12:14 +0530

    NSOL-6013 ONTAP SSL verification

--- netapp_dataops_traditional/netapp_dataops/mcp_server/config.py
+++ netapp_dataops_traditional/netapp_dataops/mcp_server/config.py
@@ -15,14 +15,19 @@ def load_credentials():
         FileNotFoundError: If the config.json file is not found in any expected location.
         ValueError: If required keys are missing from the configuration.
     """
+
+    # Path to the config.json credentials file in the user's home directory
     credentials_path = os.path.expanduser('~/.netapp_dataops/config.json')
 
+    # If path not found, raise an error that config.json file doesn't exist
     if not os.path.exists(credentials_path):
         raise FileNotFoundError("Credentials file 'config.json' not found in the ~/.netapp_dataops directory.")
 
+    # Opens the file specified, reads its contents, parses the contents as JSON, and stores the resulting data
     with open(credentials_path, 'r') as f:
         config = json.load(f)
 
+    # List of top-level keys required in the config
     required_keys = [
         'connectionType',
         'hostname',
@@ -37,6 +42,7 @@ def load_credentials():
         'defaultAggregate',
     ]
 
+    # Ensures all required top-level keys are present
     for key in required_keys:
         if key not in config:
             raise ValueError(f"Missing required key '{key}' in 'config.json' file.")
